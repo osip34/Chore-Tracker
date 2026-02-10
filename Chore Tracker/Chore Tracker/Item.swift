0e533c6485709c65a44295777e38ced9774e55c1
aosipchuk <aosipchuk@griddynamics.com>
2026-02-10 17:29:31 +0200

    First commit -> project initialized

---
+++ Chore Tracker/Chore Tracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Chore Tracker
+//
+//  Created by Andrii Osipchuk on 10.02.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
