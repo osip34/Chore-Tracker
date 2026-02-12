a47a8bdc3c851e3976e8dc9a98c08359dbde76e1
aosipchuk <aosipchuk@griddynamics.com>
2026-02-12 12:37:39 +0200

    Created file structure

---
+++ Chore Tracker/Chore Tracker/Data/Firebase/FirebaseTaskRepository.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseTaskRepository.swift
+//  Chore Tracker
+//
+//  Created by Andrii Osipchuk on 11.02.2026.
+//
+
+import Foundation
---
+++ Chore Tracker/Chore Tracker/Data/Firebase/FirebaseUserRepository.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseUserRepository.swift
+//  Chore Tracker
+//
+//  Created by Andrii Osipchuk on 11.02.2026.
+//
+
+import Foundation
