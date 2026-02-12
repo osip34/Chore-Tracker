521927f39911f2d9b2a61e53f176e3d03bad7b31
aosipchuk <aosipchuk@griddynamics.com>
2026-02-12 16:51:55 +0200

    Add Firebase pod

--- Chore Tracker/Chore Tracker/App/Chore_TrackerApp.swift
+++ Chore Tracker/Chore Tracker/App/Chore_TrackerApp.swift
@@ -7,9 +7,15 @@
 
 import SwiftUI
 import SwiftData
+import Firebase
 
 @main
 struct Chore_TrackerApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
